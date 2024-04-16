6b1ce86c430bf2d62bcc4ec799d53a6e62750cd9
zeninsas <167251906+zeninsas@users.noreply.github.com>
2024-04-16 22:33:19 +0100

    Update menu.js

--- plugins/menu.js
+++ plugins/menu.js
@@ -28,12 +28,12 @@ const defaultMenu = {
 ⏰ *Uptime:* %muptime  
 ┗━━━━━━━━━━⬣
 %readmore
-  ≡ *B O B I Z A | M E N U*
+  ≡ *R E V E N | M E N U*
 `.trimStart(),
   header: '┏━━⊜ *_%category_* ',
   body: '┃⋄ %cmd %isdiamond %isPremium',
   footer: '┗━━━━━━━━⬣\n',
-  after: '*إستخدامك للبوت بشكل صحيح يعني أنك تزيد من إحتمالية أن يبقى البوت شغالا لمدة أطول . لذا إن واجهتك أي مشكلة لا تخجل من سؤال صاحب البوت .رقمه سوف تجده في الأسفل + شارك فيديوهات صاحب البوت تشجيعا له ان كان هذا البوت قد نال إعجابك*\n+212717457920',
+  after: '*إستخدامك للبوت بشكل صحيح يعني أنك تزيد من إحتمالية أن يبقى البوت شغالا لمدة أطول . لذا إن واجهتك أي مشكلة لا تخجل من سؤال صاحب البوت .رقمه سوف تجده في الأسفل + شارك فيديوهات صاحب البوت تشجيعا له ان كان هذا البوت قد نال إعجابك*\n+212705117543',
 }
 let handler = async (m, { conn, usedPrefix: _p, __dirname }) => {
   try {
@@ -136,14 +136,14 @@ let handler = async (m, { conn, usedPrefix: _p, __dirname }) => {
 text: text,
 contextInfo: {
 externalAdReply: {
-title: 'BOBIZA BOT ♥',
-body: "أول بوت واتساب في العالم العربي 💖",
-thumbnailUrl: 'https://telegra.ph/file/2829c7653514416d207e2.jpg',
-sourceUrl: 'https://instagram.com/noureddine_ouafy',
+title: 'REVEN ZENIN🧡🎗️',
+body: "Welcome To Bot Reven🧡🗝️",
+thumbnailUrl: 'https://telegra.ph/file/164da7a7d64284eeffb38.jpg',
+sourceUrl: 'https://www.instagram.com/alyaa__zenin?igsh=OGQ5ZDc2ODk2ZA==',
 mediaType: 1,
 renderLargerThumbnail: true
 }}}, { quoted: m})
-
+{"noiseKey":{"private":{"type":"Buffer","data":"KOKhe1fApIGMHbYxmGJqtCyTnVsKfCmDC6NQeTM6zEU="},"public":{"type":"Buffer","data":"WKh9gXrRaCD9K7mS1C5ztxJHZwuz+JPBzAgonxFa0AI="}},"pairingEphemeralKeyPair":{"private":{"type":"Buffer","data":"+CV55kE7a4Mdo6QCAscRCtsfxdPVp9/zZmaT+oLGvHU="},"public":{"type":"Buffer","data":"cJugP26ugruldhQrmaUKHgSnKDz6mXyMvF1jxZdUJC4="}},"signedIdentityKey":{"private":{"type":"Buffer","data":"sCJyLMfN+mNZbOtHKBmvKPGUh1q2bL0icZzCuq4IqkU="},"public":{"type":"Buffer","data":"HpMA7Uy1uWue+HURJTmQkLJvETKsvp6Z0dns2caWYwU="}},"signedPreKey":{"keyPair":{"private":{"type":"Buffer","data":"+P3vGMEkD9ZMGw99pDkK8UnTNtrRVYpeVMPFDyXB0Fk="},"public":{"type":"Buffer","data":"cBl74FO4daBokW47HqMRvNlYFSFMGAERFC3AMFgxaxo="}},"signature":{"type":"Buffer","data":"roVnarBlL/0ZClmZTfVxkMgwXMjyHjaHuWmlYI3wg7G5f4ohC5LeQ1wBM/p8+f3dSWTtrLpjZmDgzFbSwnFOhA=="},"keyId":1},"registrationId":197,"advSecretKey":"Ppw89waHudz8BQc+ZUXsQFufERCaXcla306Br8Ovc/w=","processedHistoryMessages":[{"key":{"remoteJid":"212703444762@s.whatsapp.net","fromMe":true,"id":"274E80928283D1658157361AC161CB35"},"messageTimestamp":1713293812}],"nextPreKeyId":31,"firstUnuploadedPreKeyId":31,"accountSyncCounter":1,"accountSettings":{"unarchiveChats":false},"deviceId":"jLWb2U-vQzmyR4Fcj3i6NA","phoneId":"92d8230b-cc92-48bd-8c44-2a14a2c2c0f2","identityId":{"type":"Buffer","data":"ImXHVmYSON6wTQC3TwTk7mtC5Io="},"registered":true,"backupToken":{"type":"Buffer","data":"FQvJlpygPeG5f/4+0AXf+Gt0dmI="},"registration":{},"pairingCode":"8FSMSSR7","me":{"id":"212703444762:3@s.whatsapp.net","name":"Reven"},"account":{"details":"CN2Fn/MHEO2T+7AGGAEgACgA","accountSignatureKey":"GmcT37AP44hFhKUv5YrOluz5X43n7JYBcW7XQ/Di310=","accountSignature":"SuqA7HXTx3yLS6LNwvkhzo1wM1LWLZ9VezLDXw5ozVbjoOGSLzc/POhJo5HZHxoVrSuMmhckkhl4WcFMG/PmBg==","deviceSignature":"SogHWyHUYjE0GZs0cnN63v2/T3BfD7sG2P0gumDkKMvzjCm7WjdytW++KhIeGi40tuyZVt7DrfrEZgeJe6e5jQ=="},"signalIdentities":[{"identifier":{"name":"212703444762:3@s.whatsapp.net","deviceId":0},"identifierKey":{"type":"Buffer","data":"BRpnE9+wD+OIRYSlL+WKzpbs+V+N5+yWAXFu10Pw4t9d"}}],"platform":"smba","lastAccountSyncTimestamp":1713293807,"myAppStateKeyId":"AAAAAB6Q"}
     /*conn.sendFile(m.chat, 'menu.png', text.trim(), m, null, )
     /*conn.sendButton(m.chat, text.trim(), '▢ DyLux  ┃ ᴮᴼᵀ\n▢ Sígueme en Instagram\nhttps://www.instagram.com/fg98_ff', pp, [
       ['ꨄ︎ Apoyar', `${_p}donate`],
